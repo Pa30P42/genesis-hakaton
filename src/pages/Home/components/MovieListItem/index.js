0bfbf9969f69354ff27fdd02c40b59d0d6ecb6da
MarinaSergeyeva <morrrinad@gmail.com>
2021-10-23 19:55:10 +0300

    save

--- src/pages/Home/components/MovieListItem/index.js
+++ src/pages/Home/components/MovieListItem/index.js
@@ -4,23 +4,21 @@ import './style.scss';
 const MovieListItem = ({ item }) => {
   return (
     <>
-      {/* <div className="filmTitle">{item.title}</div> */}
-
-      <div className="filmContainer">
-        <div className={style.infoWrapper}>
-          <p> {item.title}</p>
-          <p className={style.artistName} onClick={redirectToArtistPage}>
-            {artist}
-          </p>
-        </div>
-        <img src={picture} alt="artist pic" width="200" />
+      <div className="filmContainer trendingListItem">
+        <p className="filmTitle"> {item.title}</p>
+        <p
+        // onClick={redirectToArtistPage}
+        >
+          {item.overview}
+        </p>
+        <img src={item.backdrop_path} alt="film pic" width="200" />
         <a
-          href={url}
+          // href={url}
           target="_blank "
           rel="noopener noreferrer"
-          className={style.trackLink}
+          // className={style.trackLink}
         >
-          Listen to me now
+          Link
         </a>
       </div>
     </>
