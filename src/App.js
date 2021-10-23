e75e98eea08c476cdc1d9bd7c81e6e808c6a5c47
Pa30P42 <hajiev93@gmail.com>
2021-10-23 12:39:12 +0300

    add dependancies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +14,7 @@ function App() {
             className="App-link"
             href="https://reactjs.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
@@ -27,8 +22,7 @@ function App() {
             className="App-link"
             href="https://redux.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
@@ -36,8 +30,7 @@ function App() {
             className="App-link"
             href="https://redux-toolkit.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Redux Toolkit
           </a>
           ,<span> and </span>
@@ -45,8 +38,7 @@ function App() {
             className="App-link"
             href="https://react-redux.js.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             React Redux
           </a>
         </span>
