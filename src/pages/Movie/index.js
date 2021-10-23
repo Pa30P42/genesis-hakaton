84a12d7436789aaaaef7277da64bf34c5fe9c992
Veronika <nesteryuk15@gmail.com>
2021-10-23 17:20:26 +0300

    some change

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,9 +1,83 @@
 import React from 'react';
+import "./style.scss"
 
 const Movie = () => {
   return (
     <div>
-      <h2>Movie</h2>
+      <div>
+     <section className="product-card">
+        <div className="wrapper">
+            <div className="poster-left">
+              <img src="#"/>
+            </div>
+            <div className="poster-info">
+                <div className="poster-info_container">
+                    <div className="movie-title"></div>
+                    <div className="movie-descr">
+                        <h3 className="overview"> Overview</h3>
+                        <div className="overview-content-dynamic"> </div>
+                        <ul className="overview-actors">
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+
+                            <li className="profile">
+                                <p className="profile-name">
+                                  <a></a>
+                                </p>
+                                <p className="character">
+                                  <a></a>
+                                </p>
+                            </li>
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+
     </div>
   );
 };
